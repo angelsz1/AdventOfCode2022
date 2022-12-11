ce8b775bd280ebf3bffdf5eb2cd09bdc8b27035f
angelsz1 <102835960+angelsz1@users.noreply.github.com>
2022-12-11 04:50:17 -0300

    Added Advent 5 and 6

---
+++ Advent11/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Advent11
+{
+    class Advent11
+    {
+        public static void Main(string[] args)
+        {
+            float a = 5.5f;
+            Console.WriteLine(Math.Floor(a));
+        }
+    }
+}
+
---
+++ Advent6/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Advent6
+{
+    class Advent6
+    {
+        public static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(Advent2.Advent2.Path("real_input_6.in"));
+            Console.WriteLine(Solution(sr));
+        }
+
+        public static int Solution(StreamReader streamReader)
+        {
+            string line = streamReader.ReadLine();
+            List<char> charSoFar = new List<char>();
+
+            for (int i = 0; i < line.Length - 14; i++)
+            {
+                bool repe = false;
+                for (int j = i; j < i + 14; j++)
+                {
+                    //Console.WriteLine(line[j]);
+                    if (!charSoFar.Contains(line[j]))
+                        charSoFar.Add(line[j]);
+                    else
+                        repe = true;
+                }
+
+                if (!repe)
+                    return i + 14;
+                charSoFar.Clear();
+
+            }
+
+            return 0;
+        }
+    }
+}
+
