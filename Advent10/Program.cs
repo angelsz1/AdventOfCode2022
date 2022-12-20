2fb7c5d282ed71537d43185e72be7accbf55cef7
Angel <eliasszust@gmail.com>
2022-12-20 05:26:40 -0300

    Added Advent10 and Advent11

---
+++ Advent10/Program.cs
@@ -0,0 +1,158 @@
+﻿namespace Advent10
+{
+    class Advent10
+    {
+        public static void Main(string[] args)
+        {
+            Solution s = new Solution("real_input_10.in");
+            
+            Console.WriteLine(s.Solve());
+        }
+    }
+
+    class Solution
+    {
+        private StreamReader sr = null;
+        private CPU cpu;
+        private int SumOfSignals = 0;
+
+        public Solution(string path)
+        {
+            sr = new StreamReader(Advent2.Advent2.Path(path));
+            cpu = new CPU();
+        }
+
+        private string Read()
+        {
+            return sr.EndOfStream ? "EOF" : sr.ReadLine();
+        }
+
+        public int Solve()
+        {
+            string line = Read();
+
+            while (line != "EOF")
+            {
+                string[] cmd = line.Split(' ');
+                string ret;
+                if (cmd[0] == "addx")
+                    ret = cpu.addx(int.Parse(cmd[1]));
+                else
+                    ret = cpu.noop();
+
+                if (ret != "NO_RET")
+                    SumOfSignals += int.Parse(ret);
+                line = Read();
+            }
+            
+            cpu.ShowCRT();
+
+            return SumOfSignals;
+        }
+        
+        
+        
+    }
+
+
+    class CRT
+    {
+        private char[,] matrix = new char[6,40];
+        private int internalClock = 0;
+        private int rowSelector = 0;
+
+        public void Draw(int reg_x)
+        {
+            matrix[rowSelector, internalClock] = CheckValues(reg_x) ? '#' : ' ';
+            Console.WriteLine(internalClock);
+            internalClock = (internalClock + 1) % 40;
+            rowSelector = internalClock == 0 ? rowSelector + 1 : rowSelector;
+        }
+
+        private bool CheckValues(int x)
+        {
+            if (x == 0 || x == 39)
+                if (x == 0)
+                    return internalClock == x || internalClock + 1 == x || internalClock + 2 == x;
+                else
+                    return internalClock == x || internalClock - 1 == x || internalClock - 2 == x;
+            return internalClock == x || internalClock - 1 == x || internalClock + 1 == x;
+        }
+
+        public void Show()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 40; j++)
+                {
+                    Console.Write(matrix[i,j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+
+    class CPU
+    {
+        private int clock = 0;
+        private CRT crt;
+
+        private int reg_x = 1;
+
+        public CPU()
+        {
+            crt = new CRT();
+        }
+
+        public void ShowCRT()
+        {
+            crt.Show();
+        }
+
+        private void IncClock()
+        {
+            clock++;
+            crt.Draw(reg_x);
+        }
+        public string addx(int v)
+        {
+            string ret = "NO_RET";
+
+            IncClock();
+
+            if (CheckIfReport())
+                ret = (reg_x * clock).ToString();
+
+            IncClock();
+            
+            
+            if (CheckIfReport())
+                ret = (reg_x * clock).ToString();
+            
+            reg_x += v;
+
+            return ret;
+
+        }
+
+        public string noop()
+        {
+            string ret = "NO_RET";
+
+            IncClock();
+            
+            if (CheckIfReport())
+                ret = (reg_x * clock).ToString();
+
+            return ret;
+        }
+
+        private bool CheckIfReport()
+        {
+            return clock == 20 || clock == 60 || clock == 100 || clock == 140 || clock == 180 || clock == 220;
+        }
+        
+    }
+}
+
--- Advent11/Program.cs
+++ Advent11/Program.cs
@@ -4,8 +4,246 @@
     {
         public static void Main(string[] args)
         {
-            float a = 5.5f;
-            Console.WriteLine(Math.Floor(a));
+            Solution s = new Solution("real_input_11.in");
+            Console.WriteLine("Modulus = " + Solution.MODULUS);
+            Console.WriteLine(s.Solve());
+        }
+    }
+
+    class Solution
+    {
+        private StreamReader sr;
+        private List<Monkey> monkeys;
+        public static int MODULUS=1;
+        public Solution(string path)
+        {
+            sr = new StreamReader(Advent2.Advent2.Path(path));
+            monkeys = new List<Monkey>();
+            SetMonkeys();
+        }
+
+        public long Solve()
+        {
+            
+            for (int i = 0; i < 10000; i++) // 10k rondas
+            {
+                foreach (Monkey monkey in monkeys)
+                {
+                    int inspect = monkey.Inspect();
+                    while (inspect != -1)
+                    {
+                        Throw(monkey.GetNumber(), inspect);
+                        inspect = monkey.Inspect();
+                    }
+                }    
+            }
+
+            List<int> inspections = new List<int>();
+            foreach (Monkey m in monkeys)
+            {
+                inspections.Add(m.GetInspections());
+            }
+
+            long max = inspections.Max();
+            inspections.Remove(inspections.Max());
+
+
+            return max * inspections.Max();
+        }
+
+        /**
+         *          File structure
+         * Monkey NUMBER:
+         * Starting items: [NUMBER], [NUMBER]...
+         * Operation : new = old +/*\/ [NUMBER][OLD]
+         * Test : divisible by NUMBER
+         * If true : throw to monkey NUMBER
+         * If false : throw to monkey NUMBER
+         * LINE JUMP
+         */
+        private void SetMonkeys()
+        {
+            string line = Read();
+
+            while (line != "EOF")
+            {
+                Monkey curMonkey = new Monkey(int.Parse(line[7].ToString()));
+                string[] items = Read().Substring(18).Split(',');
+                foreach (string s in items)
+                {
+                    curMonkey.AddItem(new Item(int.Parse(s)));
+                }
+
+                string operation = Read().Split(':')[1].Split('=')[1];
+                string[] operations = operation.Split(' ');
+                curMonkey.SetOperation(operations);
+                
+                curMonkey.SetTest(int.Parse(Read().Split(':')[1].Split(' ')[3]));
+                
+                curMonkey.SetIfTrue(int.Parse(Read().Split(':')[1].Split(' ')[4]));
+                curMonkey.SetIfFalse(int.Parse(Read().Split(':')[1].Split(' ')[4]));
+
+                Console.WriteLine("This monkeys test " + curMonkey.GetTest());
+                MODULUS *= curMonkey.GetTest();
+                
+                
+                Read(); //line jump
+                line = Read(); //Next monkey or EOF
+                
+                monkeys.Add(curMonkey);
+            }
+        }
+
+        private string Read()
+        {
+            return sr.EndOfStream ? "EOF" : sr.ReadLine();
+        }
+
+        private void Throw(int from, int to)
+        {
+            monkeys[to].AddItem(monkeys[from].RemoveItem());
+        }
+    }
+
+
+    class Monkey
+    {
+        private Queue<Item> items;
+        private int number;
+        private string[] operation;
+        private int ifTrue;
+        private int ifFalse;
+        private int test;
+        private int inspectionCounter = 0;
+
+        public Monkey(int number)
+        {
+            this.number = number;
+            items = new Queue<Item>();
+        }
+
+        public void AddItem(Item itemToAdd)
+        {
+            items.Enqueue(itemToAdd);
+        }
+
+        public Item RemoveItem()
+        {
+            return items.Dequeue();
+        }
+
+        public void SetOperation(string[] operation)
+        {
+            this.operation = operation;
+        }
+
+        public void SetIfTrue(int iftrue)
+        {
+            ifTrue = iftrue;
+        }
+
+        public void SetIfFalse(int ifFalse)
+        {
+            this.ifFalse = ifFalse;
+        }
+
+        public void SetTest(int test)
+        {
+            this.test = test;
+        }
+
+        public int GetNumber()
+        {
+            return number;
+        }
+
+        public int GetTest()
+        {
+            return test;
+        }
+
+        public int GetInspections()
+        {
+            return inspectionCounter;
+        }
+
+        public int Inspect()
+        {
+            if (items.Count == 0)
+                return -1;
+            
+            Item item = items.Peek();
+            inspectionCounter++;
+
+            long operationNumber;
+
+            if (operation[3] == "old")
+                operationNumber = item.GetWorryLevel();
+            else
+                operationNumber = int.Parse(operation[3]);
+
+
+            switch (operation[2])
+            {
+                case "+":
+                    item.SetWorryLevel(item.GetWorryLevel() + operationNumber);
+                    break;
+                case "*":
+                    item.SetWorryLevel(item.GetWorryLevel() * operationNumber);
+                    break;
+            }
+
+            item.SetWorryLevel(item.GetWorryLevel() % Solution.MODULUS);
+
+            return item.GetWorryLevel() % test == 0 ? ifTrue : ifFalse;
+        }
+
+        public override string ToString()
+        {
+            string monk = "Monkey " + number;
+
+            monk += "\nStarting items: ";
+            foreach (Item i in items)
+            {
+                monk += i.ToString() + " ";
+            }
+
+            monk += "\nOperation: new = " + "old"+ " " +operation[2] + " " + operation[3];
+
+            monk += "\nTest: divisible by " + test;
+
+            monk += "\n\tIf True: Throw to monkey " + ifTrue;
+            monk += "\n\tIf False: Throw to monkey " + ifFalse;
+
+
+            return monk;
+
+
+        }
+    }
+
+    class Item
+    {
+        private long worryLevel;
+
+        public Item(long w)
+        {
+            worryLevel = w;
+        }
+
+        public void SetWorryLevel(long worry)
+        {
+            worryLevel = worry;
+        }
+
+        public long GetWorryLevel()
+        {
+            return worryLevel;
+        }
+
+        public override string ToString()
+        {
+            return worryLevel.ToString();
         }
     }
 }
